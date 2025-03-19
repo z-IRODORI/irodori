4c9a9363d637beac957ac24e6afb051f0f087385
hamadayuuki <yuukirinnma@gmail.com>
2025-03-19 16:19:18 +0900

    Init project

---
+++ irodori/irodoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  irodoriApp.swift
+//  irodori
+//
+//  Created by 濵田　悠樹 on 2025/03/19.
+//
+
+import SwiftUI
+
+@main
+struct irodoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ irodoriTests/irodoriTests.swift
@@ -0,0 +1,17 @@
+//
+//  irodoriTests.swift
+//  irodoriTests
+//
+//  Created by 濵田　悠樹 on 2025/03/19.
+//
+
+import Testing
+@testable import irodori
+
+struct irodoriTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
