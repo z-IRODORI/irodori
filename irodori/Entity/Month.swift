f0c398fb226cd3e1e3624586628a39d6be754082
hamadayuuki <yuukirinnma@gmail.com>
2025-06-21 22:59:18 +0900

    Add Month

---
+++ irodori/Entity/Month.swift
@@ -0,0 +1,32 @@
+//
+//  Month.swift
+//  irodori
+//
+//  Created by 濵田　悠樹 on 2025/06/21.
+//
+
+import Foundation
+
+struct Month: Identifiable {
+    var id: String = UUID().uuidString
+    var month: String = ""
+    var monthOfTheYear: Int = 0
+    var year: Int = 0
+
+    var amountOfDays:Int {
+        let dateComponents = DateComponents(year: year, month: monthOfTheYear)
+        let calendar = Calendar.current
+        let date = calendar.date(from: dateComponents)!
+
+        let range = calendar.range(of: .day, in: .month, for: date)!
+        let numDays = range.count
+        return numDays
+    }
+    var spacesBeforeFirst:Int {
+        let dateComponents = DateComponents(year: year, month: monthOfTheYear)
+        let calendar = Calendar.current
+        let date = calendar.date(from: dateComponents)!
+
+        return date.dayNumberOfWeek()!
+    }
+}
