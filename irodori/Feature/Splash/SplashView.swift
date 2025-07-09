d8719a1151d27e412f7d8b8c340fc56d0f72d4cb
hamadayuuki <yuukirinnma@gmail.com>
2025-07-10 00:00:06 +0900

    Update splash view

--- irodori/Feature/Splash/SplashView.swift
+++ irodori/Feature/Splash/SplashView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct SplashView: View {
     let viewModel: SplashViewModel = .init()
 
+    init() {
+        viewModel.updateState()
+    }
+
     var body: some View {
         // TODO: - スプラッシュ画面を実装して、そこで画面遷移を実行
         // 既存の実装では 利用規約を確認したこと や ユーザー情報を入力したこと を検知できないため画面遷移できない
