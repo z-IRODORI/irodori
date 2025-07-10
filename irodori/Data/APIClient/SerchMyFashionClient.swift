8e0336c1f309c32001f32c085547c0ec5bbf959d
hamadayuuki <yuukirinnma@gmail.com>
2025-07-10 21:17:12 +0900

    Rename apiclient

---
+++ irodori/Data/APIClient/SerchMyFashionClient.swift
@@ -0,0 +1,43 @@
+//
+//  SerchMyFashionClient.swift
+//  irodori
+//
+//  Created by yuki.hamada on 2025/04/04.
+//
+
+import UIKit
+
+final class SerchMyFashionClient {
+    func postImage(image: UIImage) async throws -> Result<PredictResponse, Error> {
+        let baseURL = "https://irodori.click"
+        let endpoint = "predict"
+        let url = URL(string: "\(baseURL)/\(endpoint)")!
+
+        // UIImageをJPEGデータに変換し、Base64エンコード
+        guard let jpegData = image.jpegData(compressionQuality: 0.8) else {
+            throw URLError(.badURL)
+        }
+        let base64String = jpegData.base64EncodedString()
+
+
+        let requestBody = PredictRequest(image_base64: base64String)
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try JSONEncoder().encode(requestBody)
+
+        do {
+            // URLSessionでリクエストを送信
+            let (data, _) = try await URLSession.shared.data(for: request)
+            // JSONレスポンスをデコード
+            let response = try JSONDecoder().decode(PredictResponse.self, from: data)
+            return .success(response)
+        } catch {
+            print(error.localizedDescription)
+            return .failure(error)
+        }
+    }
+}
+
+
