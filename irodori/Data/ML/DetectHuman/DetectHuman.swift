c9c5a6eaea6e34ef93cb3ff795432733cfc90fdc
hamadayuuki <yuukirinnma@gmail.com>
2025-07-28 01:06:59 +0900

    Impl detect human using Vision

---
+++ irodori/Data/ML/DetectHuman/DetectHuman.swift
@@ -0,0 +1,30 @@
+//
+//  DetectHuman.swift
+//  irodori
+//
+//  Created by yuki.hamada on 2025/07/28.
+//
+
+import Vision
+import UIKit
+
+struct DetectHuman {
+    let request: VNDetectHumanRectanglesRequest
+    init() {
+        request = VNDetectHumanRectanglesRequest()
+        request.revision = VNDetectHumanRectanglesRequestRevision2
+        request.upperBodyOnly = false  // 全身検出
+    }
+
+    /// 人間の検出有無に応じて Bool を返す
+    func detect(inputCIImage: CIImage) -> Bool {
+        do {
+            let handler = VNImageRequestHandler(ciImage: inputCIImage, options: [:])
+            try handler.perform([request])
+            guard let obs = request.results?.first as? VNDetectedObjectObservation else { return false }
+            return obs.confidence > 0.6
+        } catch {
+            return false
+        }
+    }
+}
