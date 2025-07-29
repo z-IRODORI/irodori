285d98b0905a0ee50b309058a6bfac72b5b6b703
hamadayuuki <yuukirinnma@gmail.com>
2025-07-29 15:11:47 +0900

    Impl MLError

---
+++ irodori/Data/ML/MLError.swift
@@ -0,0 +1,40 @@
+//
+//  MLError.swift
+//  irodori
+//
+//  Created by yuki.hamada on 2025/07/28.
+//
+
+import Foundation
+
+enum MLError {
+    // DetectHuman
+    case notHuman
+
+    // Segment
+    case notTops
+    case notBottoms
+    case notTopsAndBottoms
+
+    var title: String {
+        switch self {
+        case .notHuman: return "全身認識エラー"
+        case .notTops: return "トップス検出エラー"
+        case .notBottoms: return "ボトムス検出エラー"
+        case .notTopsAndBottoms: return "トップス&ボトムス検出エラー"
+        }
+    }
+
+    var errorDescription: String {
+        switch self {
+        case .notHuman:
+            return "人物全体が写っていないか、人物が画像内に存在しない可能性があります。\n再度撮影してください。"
+        case .notTops:
+            return "画像内の人物からトップス領域を正しく検出できませんでした。"
+        case .notBottoms:
+            return "画像内の人物からボトムス領域を正しく検出できませんでした。"
+        case .notTopsAndBottoms:
+            return "トップスおよびボトムスの両方の領域を検出できませんでした。"
+        }
+    }
+}
