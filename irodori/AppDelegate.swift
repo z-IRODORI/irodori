5d6556a64dc4fd7c8584e9f2ebd0690bffb2bb93
hamadayuuki <yuki.hamada@zozo.com>
2025-11-09 22:26:07 +0900

    Install Firebase Analytics

---
+++ irodori/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  irodori
+//
+//  Created by yuki.hamada on 2025/11/09.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
