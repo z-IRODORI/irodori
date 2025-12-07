d336865b32c8003b48dfd2a66f46060ae5ddbb4c
hamadayuuki <yuki.hamada@zozo.com>
2025-12-07 16:26:46 +0900

    Impl choise gemini model

--- irodori/Data/APIClient/Request/FashionReviewRequest.swift
+++ irodori/Data/APIClient/Request/FashionReviewRequest.swift
@@ -11,6 +11,7 @@ struct FashionReviewRequest: Encodable {
     let user_id: String
     let user_token: String
     let file: Data   // 全身画像
+    let model: String? = "gemini-3-pro-preview"   // "gemini-2.5-flash" or "gemini-2.5-flash-lite" or "gemini-3-pro-preview"
 }
 
 extension FashionReviewRequest {
@@ -19,6 +20,7 @@ extension FashionReviewRequest {
         parameters["user_id"] = user_id
         parameters["user_token"] = user_token
         parameters["file"] = file
+        parameters["model"] = model
         return parameters
     }
 }
